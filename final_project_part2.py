935abf3cceac2afc6404d580480e4b5e2cd17680
Selim Abdel Razek <srazek2003@hotmail.com>
2023-04-15 18:52:34 -0400

    implemented a_star

---
+++ final_project_part2.py
@@ -0,0 +1,28 @@
+import min_heap
+from graph import Graph
+
+def a_star(G, s, d, h):
+    pred = {}
+    dist = {}
+    Q = min_heap.MinHeap([])
+    nodes = list(G.adj.keys())
+
+    #Initialize priority queue/heap and distances
+    for node in nodes:
+        Q.insert(min_heap.Element(node, float("inf")))
+        dist[node] = float("inf")
+    Q.decrease_key(s, 0)
+
+    #Meat of the algorithm
+    while not Q.is_empty():
+        current_element = Q.extract_min()
+        current_node = current_element.value
+        if current_node == d:
+            break
+        dist[current_node] = current_element.key
+        for neighbour in G.adj[current_node]:
+            if dist[current_node] + G.w(current_node, neighbour) < dist[neighbour]:
+                Q.decrease_key(neighbour, dist[current_node] + G.w(current_node, neighbour) + h(neighbour))
+                dist[neighbour] = dist[current_node] + G.w(current_node, neighbour)
+                pred[neighbour] = current_node
+    return pred, dist
