456afed610d46fae4186e36641f1db2e21d687ca
Selim Abdel Razek <srazek2003@hotmail.com>
2023-04-16 23:26:35 -0400

    thing

--- london.py
+++ london.py
@@ -2,6 +2,7 @@ import csv
 import math
 from heuristic_graph import HeuristicGraph
 coords = {}
+linesConnections = {}
 lines = {}
 def create_tube_graph():
     tube_graph = HeuristicGraph()
@@ -12,6 +13,7 @@ def create_tube_graph():
         for line in tube_reader:
             tube_graph.add_node(int(line['id']))
             coords[int(line['id'])] = [float(line['latitude']), float(line['longitude'])]
+            lines[int(line['id'])] = int(line['total_lines'])
     with open('csv_files/london_connections.csv', mode='r') as csvfile:
         connect_reader = csv.DictReader(csvfile)
         for line in connect_reader:
@@ -19,7 +21,7 @@ def create_tube_graph():
             s2 = int(line['station2'])
             line = int(line['line'])
             tube_graph.add_edge(s1, s2, calc_dist(coords[s1], coords[s2]))
-            lines[(s1, s2)] = line
+            linesConnections[s1] = s2
     #the following are for testing and can be removed later:
     print(f"number of nodes: {tube_graph.get_num_of_nodes()}")
     print(f"weight between Charing Cross and Embankment (between 100m and 150m IRL): {calc_dist(coords[49], coords[87])}")
