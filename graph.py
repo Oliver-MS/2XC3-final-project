fe1065387e94b77c1b5d9034ed7c4d095fbe8144
Oliver Metz-Strauss <oliverms1208@gmail.com>
2023-04-16 22:46:48 -0400

    fixed graph so all methods are abstract, added spf

--- graph.py
+++ graph.py
@@ -7,31 +7,22 @@ class Graph(ABC):
         self.adj = {}
         self.weights = {}
     
+    @abstractmethod
     def get_adj_nodes(self, node:int):
-        return self.adj[node]
-    
-    def add_node(self, node:int):
-        self.adj[node] = []
-    
-    def add_edge(self, start:int, end:int, w:float):
-        #assumes start is already in graph, may break if we attempt to pass a node that doesn't already exist
-        #assumes directed graph as a baseline, undirected graph would require further modification
-        if end not in self.adj[start]:
-            self.adj[start].append(end)
-        self.weights[(start, end)] = w
-    
-    def get_num_of_nodes(self) -> int:
-        return len(self.adj)
-    
-    def w(self, node:int) -> float:
-        #unsure what this is supposed to do, maybe return the node's integer value?
-        #should probably return float("inf") since the implication is that this graph is not weighted, should also take two nodes as input
         pass
     
-    def are_connected(self, start:int, end:int) -> bool:
-        if end in self.adj[start]:
-            return True
-        return False
+    @abstractmethod
+    def add_node(self, node:int):
+        pass
     
-    #def w(self, node1:int, node2:int):
-    #    return float("inf")
+    @abstractmethod
+    def add_edge(self, start:int, end:int, w:float):
+        pass
+    
+    @abstractmethod
+    def get_num_of_nodes(self) -> int:
+        pass
+    
+    @abstractmethod
+    def w(self, node1:int, node2:int) -> float:
+        pass
