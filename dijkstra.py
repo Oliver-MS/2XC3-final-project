0fabe0c67709116f73092c172d5b8d58e02d73d2
BuildTools <unconfigured@null.spigotmc.org>
2023-04-16 22:59:59 -0400

    disjktra py file

--- dijkstra.py
+++ dijkstra.py
@@ -5,12 +5,12 @@ import min_heap
 
 class Dijkstra(SPAlgorithm):
     #this super needs fixing FYI
-    def calc_sp(graph: Graph, source: int, dest: int):
-        pred = {} #Predecessor dictionary. Isn't returned, but here for your understanding
+    def calc_sp(G, source, dest):
+        pred = {} #Predecessor dictionary
         dist = {} #Distance dictionary
         Q = min_heap.MinHeap([])
-        nodes = list(graph.adj.keys())
-        
+        nodes = list(G.adj.keys())
+
         #Initialize priority queue/heap and distances
         for node in nodes:
             Q.insert(min_heap.Element(node, float("inf")))
@@ -22,9 +22,15 @@ class Dijkstra(SPAlgorithm):
             current_element = Q.extract_min()
             current_node = current_element.value
             dist[current_node] = current_element.key
-            for neighbour in graph.adj[current_node]:
-                if dist[current_node] + graph.w(current_node, neighbour) < dist[neighbour]:
-                    Q.decrease_key(neighbour, dist[current_node] + graph.w(current_node, neighbour))
-                    dist[neighbour] = dist[current_node] + graph.w(current_node, neighbour)
-                    pred[neighbour] = current_node
-        return dist
+            for neighbour in G.adj[current_node]:
+                if dist[current_node] + G.w(current_node, neighbour) < dist[neighbour]:
+                    Q.decrease_key(neighbour, dist[current_node] + G.w(current_node, neighbour))
+                    dist[neighbour] = dist[current_node] + G.w(current_node, neighbour)
+                    pred[neighbour] = current_node # add/update predecessor
+        if dest not in pred:
+            return None, None
+        path = [dest]
+        while path[-1] != source:
+            path.append(pred[path[-1]])
+        path.reverse()
+        return pred, path
