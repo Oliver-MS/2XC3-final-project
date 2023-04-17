b50752e71ba5b4666628f248371d99c779178663
BuildTools <unconfigured@null.spigotmc.org>
2023-04-16 23:34:16 -0400

    bellmanford

---
+++ bellman_ford.py
@@ -0,0 +1,31 @@
+from sp_algorithm import SPAlgorithm
+import min_heap
+#interesting question: does each implementation of SPAlgorithm have to import Graph or is there a fix to this?
+
+class Bellman_Ford(SPAlgorithm):
+    #this super needs fixing FYI
+    def calc_sp(G, source, dest):
+
+        pred = {} #Predecessor dictionary. Isn't returned, but here for your understanding
+        dist = {} #Distance dictionary
+        nodes = list(G.adj.keys())
+
+        #Initialize distances
+        for node in nodes:
+            dist[node] = float("inf")
+        dist[source] = 0
+
+        #Meat of the algorithm
+        for _ in range(G.number_of_nodes()):
+            for node in nodes:
+                for neighbour in G.adj[node]:
+                    if dist[neighbour] > dist[node] + G.w(node, neighbour):
+                        dist[neighbour] = dist[node] + G.w(node, neighbour)
+                        pred[neighbour] = node
+        if dest not in pred:
+            return None, None
+        path = [dest]
+        while path[-1] != source:
+            path.append(pred[path[-1]])
+        path.reverse()
+        return pred, path
